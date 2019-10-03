9e7c630449cee5c552061da8a758b38d4da1e53a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-03 13:28:50 -0700

    4 square first program

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,18 +6,24 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot r2d2= new Robot();
 	void go() {
+		//1.create a new Robot
+		
 		// 4. Make the robot move as fast as possible
 
 		// 5. Set the pen width to 5
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
 
+		//7.change the pen color to random
 			// 7. Set the pen color to random
 	
-			// 1. Call the drawSquare() method
-	
+			// 1. Call the drawSquare() method 
+		r2d2.penDown();
+        r2d2.setSpeed(100);		
+		r2d2.move(200);
+		r2d2.turn(90);
 			// 8. Turn the robot 90 degrees to the right
 
 	}
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,18 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
-	}
+Robot 	r2d2=new Robot();
+r2d2.setSpeed(100);
+r2d2.penDown();
+;
+for( int i=0; i<4 ;i++ ) {
+r2d2.move(200);	
+r2d2.turn(90);	
+
 }
+			
+	}
+	
+	
+}
+
